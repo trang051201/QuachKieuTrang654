03245a1d369be9d115063127300ac08cdafb44bc
trang051201 <trangtrau201@gmail.com>
2023-12-29 13:22:18 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace QuachKieuTrang654.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
